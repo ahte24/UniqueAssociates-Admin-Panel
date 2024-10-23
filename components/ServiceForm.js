69ffa104652312d22a8553bbedef5dc8bebb3df0
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2024-10-24 00:59:44 +0530

    final commit

--- components/ServiceForm.js
+++ components/ServiceForm.js
@@ -4,6 +4,9 @@ import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import axios from "axios";
 import endpoint from "@/utils/endpoint";
+import "react-toastify/dist/ReactToastify.css";
+import { CircleChevronRight } from "lucide-react";
+import Loader from "@/components/Loader";
 
 export default function ServiceForm({
 	categories,
@@ -193,7 +196,16 @@ export default function ServiceForm({
 					},
 				}
 			);
-
+			toast.success("Service added successfully.", {
+				position: "top-right",
+				autoClose: 1000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				theme: "light",
+				transition: Bounce,
+			});
 			console.log("Service added successfully:", response.data);
 
 			// Reset form fields after submission if needed
@@ -230,6 +242,16 @@ export default function ServiceForm({
 				"Error adding service:",
 				error.response?.data || error.message
 			);
+			toast.error("Error adding service.", {
+				position: "top-right",
+				autoClose: 1000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				theme: "light",
+				transition: Bounce,
+			});
 		}
 	};
 
@@ -247,223 +269,242 @@ export default function ServiceForm({
 	}, []);
 
 	return (
-		<motion.div
-			className="w-[70%] mx-auto bg-white rounded-lg shadow-md p-6 mt-8"
-			initial={{ opacity: 0, y: 50 }}
-			animate={{ opacity: 1, y: 0 }}
-			transition={{ duration: 0.5 }}
-		>
-			<form onSubmit={handleSubmit}>
-				<div className="mb-4">
-					<label className="block text-gray-700 mb-2">Category</label>
-					<select
-						className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-						value={selectedCategory}
-						onChange={(e) => setSelectedCategory(e.target.value)}
-						required
-					>
-						<option value="" disabled>
-							Select Category
-						</option>
-						{category.map((category, index) => (
-							<option key={index} value={category.id}>
-								{category.category_name}
+		<>
+			<ToastContainer
+				position="top-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="light"
+				transition={Bounce}
+			/>
+			<motion.div
+				className="w-[70%] mx-auto bg-white rounded-lg shadow-md p-6 mt-8"
+				initial={{ opacity: 0, y: 50 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.5 }}
+			>
+				<form onSubmit={handleSubmit}>
+					<div className="mb-4">
+						<label className="block text-gray-700 mb-2">Category</label>
+						<select
+							className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+							value={selectedCategory}
+							onChange={(e) => setSelectedCategory(e.target.value)}
+							required
+						>
+							<option value="" disabled>
+								Select Category
 							</option>
-						))}
-					</select>
-				</div>
-				<div className="mb-4">
-					<label className="block text-gray-700 mb-2">Service Name</label>
-					<input
-						type="text"
-						className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-						value={serviceName}
-						onChange={(e) => setServiceName(e.target.value)}
-						required
-					/>
-				</div>
-				<div className="mb-4">
-					<label className="block text-gray-700 mb-2">
-						Service Description
-					</label>
-					<textarea
-						className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-						value={serviceDescription}
-						onChange={(e) => setServiceDescription(e.target.value)}
-						required
-					/>
-				</div>
-				<div className="mb-4">
-					<label className="block text-gray-700 mb-2">Service Price</label>
-					<input
-						type="number"
-						className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-						value={servicePrice === 0 ? "" : servicePrice}
-						onChange={(e) => setServicePrice(parseFloat(e.target.value) || 0)}
-						required
-					/>
-				</div>
-
-				{/* Most Visited Checkbox */}
-				<div className="mb-4">
-					<label className="inline-flex items-center">
+							{category.map((category, index) => (
+								<option key={index} value={category.id}>
+									{category.category_name}
+								</option>
+							))}
+						</select>
+					</div>
+					<div className="mb-4">
+						<label className="block text-gray-700 mb-2">Service Name</label>
 						<input
-							type="checkbox"
-							className="form-checkbox border-2 border-gray-400 text-blue-600"
-							checked={mostVisited}
-							onChange={(e) => setMostVisited(e.target.checked)}
+							type="text"
+							className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+							value={serviceName}
+							onChange={(e) => setServiceName(e.target.value)}
+							required
 						/>
-						<span className="ml-2 text-gray-700">Most Visited</span>
-					</label>
-				</div>
+					</div>
+					<div className="mb-4">
+						<label className="block text-gray-700 mb-2">
+							Service Description
+						</label>
+						<textarea
+							className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+							value={serviceDescription}
+							onChange={(e) => setServiceDescription(e.target.value)}
+							required
+						/>
+					</div>
+					<div className="mb-4">
+						<label className="block text-gray-700 mb-2">Service Price</label>
+						<input
+							type="number"
+							className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+							value={servicePrice === 0 ? "" : servicePrice}
+							onChange={(e) => setServicePrice(parseFloat(e.target.value) || 0)}
+							required
+						/>
+					</div>
 
-				{/* Cover Image Upload */}
-				<div className="mb-4">
-					<label className="block text-gray-700 mb-2">Cover Image</label>
-					<input
-						type="file"
-						className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-						onChange={(e) => setCoverImage(e.target.files[0])}
-					/>
-				</div>
+					{/* Most Visited Checkbox */}
+					<div className="mb-4">
+						<label className="inline-flex items-center">
+							<input
+								type="checkbox"
+								className="form-checkbox border-2 border-gray-400 text-blue-600"
+								checked={mostVisited}
+								onChange={(e) => setMostVisited(e.target.checked)}
+							/>
+							<span className="ml-2 text-gray-700">Most Visited</span>
+						</label>
+					</div>
 
-				{/* Image Upload */}
-				<div className="mb-4">
-					<label className="block text-gray-700 mb-2">Image</label>
-					<input
-						type="file"
-						className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-						onChange={(e) => setImage(e.target.files[0])}
-					/>
-				</div>
+					{/* Cover Image Upload */}
+					<div className="mb-4">
+						<label className="block text-gray-700 mb-2">Cover Image</label>
+						<input
+							type="file"
+							className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+							onChange={(e) => setCoverImage(e.target.files[0])}
+						/>
+					</div>
 
-				{/* Number of Service Descriptions */}
-				<div className="mb-4">
-					<label className="block text-gray-700 mb-2">
-						Number of Service Descriptions
-					</label>
-					<input
-						type="number"
-						className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-						value={
-							numberOfServiceDescriptions === 0
-								? ""
-								: numberOfServiceDescriptions
-						}
-						onChange={(e) =>
-							handleNumberOfServiceDescriptionsChange(e.target.value)
-						}
-						min="0"
-						required
-					/>
-				</div>
+					{/* Image Upload */}
+					<div className="mb-4">
+						<label className="block text-gray-700 mb-2">Image</label>
+						<input
+							type="file"
+							className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+							onChange={(e) => setImage(e.target.files[0])}
+						/>
+					</div>
 
-				{sections.map((section, index) => (
-					<motion.div
-						key={index}
-						className="mb-6 border-2 p-4 rounded-xl border-gray-300 pt-4"
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						transition={{ duration: 0.5, delay: index * 0.2 }}
-					>
-						<div className="flex justify-between items-center mb-2">
-							<h3 className="text-lg font-bold text-gray-900">
-								Section {index + 1}
-							</h3>
-						</div>
-						<div className="mb-4">
-							<label className="block text-gray-700 mb-2">Heading</label>
+					{/* Number of Service Descriptions */}
+					<div className="mb-4">
+						<label className="block text-gray-700 mb-2">
+							Number of Service Descriptions
+						</label>
+						<input
+							type="number"
+							className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+							value={
+								numberOfServiceDescriptions === 0
+									? ""
+									: numberOfServiceDescriptions
+							}
+							onChange={(e) =>
+								handleNumberOfServiceDescriptionsChange(e.target.value)
+							}
+							min="0"
+							required
+						/>
+					</div>
+
+					{sections.map((section, index) => (
+						<motion.div
+							key={index}
+							className="mb-6 border-2 p-4 rounded-xl border-gray-300 pt-4"
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							transition={{ duration: 0.5, delay: index * 0.2 }}
+						>
+							<div className="flex justify-between items-center mb-2">
+								<h3 className="text-lg font-bold text-gray-900">
+									Section {index + 1}
+								</h3>
+							</div>
+							<div className="mb-4">
+								<label className="block text-gray-700 mb-2">Heading</label>
+								<input
+									type="text"
+									className="w-full  px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-600"
+									value={section.heading}
+									onChange={(e) =>
+										handleSectionChange(index, "heading", e.target.value)
+									}
+									required
+								/>
+							</div>
+							<div className="mb-4">
+								<label className="block text-gray-700 mb-2">Points</label>
+								<div className="flex flex-col sm:flex-row items-center mb-2">
+									<input
+										type="text"
+										className="flex-grow w-full sm:w-auto px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-600 mb-2 sm:mb-0 sm:mr-2"
+										value={newPoints[index]}
+										onChange={(e) =>
+											handleNewPointChange(index, e.target.value)
+										}
+									/>
+									<button
+										type="button"
+										className={`px-4 py-2 rounded-lg transition duration-300 w-full sm:w-auto ${
+											sections[index].points.length >= 6
+												? "bg-gray-400 cursor-not-allowed"
+												: "bg-green-500 text-white hover:bg-green-600"
+										}`}
+										onClick={() => handleAddPoint(index)}
+										disabled={sections[index].points.length >= 6}
+									>
+										Add Point
+									</button>
+								</div>
+								<ul className="mt-2 list-disc pl-6">
+									{section.points.map((point, pointIndex) => (
+										<li
+											key={pointIndex}
+											className="text-gray-600 flex justify-between items-center"
+										>
+											{point}
+											<button
+												type="button"
+												className="text-red-500 hover:text-red-700 ml-4"
+												onClick={() => handleRemovePoint(index, pointIndex)}
+											>
+												Remove
+											</button>
+										</li>
+									))}
+								</ul>
+							</div>
+						</motion.div>
+					))}
+
+					{/* Number of Documents Needed */}
+					<div className="mb-4">
+						<label className="block text-gray-700 mb-2">
+							Number of Documents Needed
+						</label>
+						<input
+							type="number"
+							className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+							value={numberOfDocuments === 0 ? "" : numberOfDocuments}
+							onChange={(e) => handleNumberOfDocumentsChange(e.target.value)}
+							min="0"
+							required
+						/>
+					</div>
+
+					{/* Document Names Inputs */}
+					{Array.from({ length: numberOfDocuments }).map((_, index) => (
+						<div key={index} className="mb-4">
+							<label className="block text-gray-700 mb-2">
+								Document Name {index + 1}
+							</label>
 							<input
 								type="text"
-								className="w-full  px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-600"
-								value={section.heading}
+								className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+								value={documentNames[index] || ""}
 								onChange={(e) =>
-									handleSectionChange(index, "heading", e.target.value)
+									handleDocumentNameChange(index, e.target.value)
 								}
 								required
 							/>
 						</div>
-						<div className="mb-4">
-							<label className="block text-gray-700 mb-2">Points</label>
-							<div className="flex flex-col sm:flex-row items-center mb-2">
-								<input
-									type="text"
-									className="flex-grow w-full sm:w-auto px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-600 mb-2 sm:mb-0 sm:mr-2"
-									value={newPoints[index]}
-									onChange={(e) => handleNewPointChange(index, e.target.value)}
-								/>
-								<button
-									type="button"
-									className={`px-4 py-2 rounded-lg transition duration-300 w-full sm:w-auto ${
-										sections[index].points.length >= 6
-											? "bg-gray-400 cursor-not-allowed"
-											: "bg-green-500 text-white hover:bg-green-600"
-									}`}
-									onClick={() => handleAddPoint(index)}
-									disabled={sections[index].points.length >= 6}
-								>
-									Add Point
-								</button>
-							</div>
-							<ul className="mt-2 list-disc pl-6">
-								{section.points.map((point, pointIndex) => (
-									<li
-										key={pointIndex}
-										className="text-gray-600 flex justify-between items-center"
-									>
-										{point}
-										<button
-											type="button"
-											className="text-red-500 hover:text-red-700 ml-4"
-											onClick={() => handleRemovePoint(index, pointIndex)}
-										>
-											Remove
-										</button>
-									</li>
-								))}
-							</ul>
-						</div>
-					</motion.div>
-				))}
+					))}
 
-				{/* Number of Documents Needed */}
-				<div className="mb-4">
-					<label className="block text-gray-700 mb-2">
-						Number of Documents Needed
-					</label>
-					<input
-						type="number"
-						className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-						value={numberOfDocuments === 0 ? "" : numberOfDocuments}
-						onChange={(e) => handleNumberOfDocumentsChange(e.target.value)}
-						min="0"
-						required
-					/>
-				</div>
-
-				{/* Document Names Inputs */}
-				{Array.from({ length: numberOfDocuments }).map((_, index) => (
-					<div key={index} className="mb-4">
-						<label className="block text-gray-700 mb-2">
-							Document Name {index + 1}
-						</label>
-						<input
-							type="text"
-							className="w-full px-4 py-2 border-2 border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-							value={documentNames[index] || ""}
-							onChange={(e) => handleDocumentNameChange(index, e.target.value)}
-							required
-						/>
-					</div>
-				))}
-
-				<button
-					type="submit"
-					className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300"
-				>
-					{isEditMode ? "Update Service" : "Add Service"}
-				</button>
-			</form>
-		</motion.div>
+					<button
+						type="submit"
+						className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300"
+					>
+						{isEditMode ? "Update Service" : "Add Service"}
+					</button>
+				</form>
+			</motion.div>
+		</>
 	);
 }
